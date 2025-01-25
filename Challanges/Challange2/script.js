56b9167da68189dc9661c4906d5b73bf14c6b6b5
nitish <mandalnitish570@gmail.com>
2025-01-25 17:14:35 +0530

    25 jan

--- Challanges/Challange2/script.js
+++ Challanges/Challange2/script.js
@@ -2,10 +2,6 @@ const toggle = document.getElementById('darkModeToggle');
 const body = document.body;
 
 // Load saved mode from localStorage
-
-
-
-
 toggle.addEventListener('click', () => {
 
 	body.classList.toggle('dark');
@@ -17,30 +13,10 @@ toggle.addEventListener('click', () => {
 	}
 });
 
+
 const darkMode = localStorage.getItem('dark-mode');
 if (darkMode === 'enabled') {
 	body.classList.add('dark');
 }
 
 
-// document.addEventListener('DOMContentLoaded', () => {
-// 	const toggle = document.getElementById('darkModeToggle');
-// 	const body = document.body;
-  
-// 	// Load saved mode from localStorage
-// 	const darkMode = localStorage.getItem('dark-mode');
-// 	if (darkMode === 'enabled') {
-// 	  body.classList.add('dark');
-// 	}
-  
-// 	// Add event listener to toggle button
-// 	toggle.addEventListener('click', () => {
-// 	  body.classList.toggle('dark');
-// 	  if (body.classList.contains('dark')) {
-// 		localStorage.setItem('dark-mode', 'enabled');
-// 	  } else {
-// 		localStorage.setItem('dark-mode', 'disabled');
-// 	  }
-// 	});
-//   });
-  
