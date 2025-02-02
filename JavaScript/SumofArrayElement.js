577fcbb4a76604249537f166bd677b942f24eba4
nitish <mandalnitish570@gmail.com>
2025-02-02 22:32:32 +0530

    problem added

---
+++ JavaScript/SumofArrayElement.js
@@ -0,0 +1,16 @@
+// node SumofArrayElement.js
+
+// SumofArrayElement.js
+
+function addnum(arr) {
+    let sum = 0;
+    for (let l = 0; l < arr.length; l++) {
+        // sum = sum + arr[l];
+        sum += arr[l];
+    }
+    return sum;
+}
+
+const arr = [1, 2, 3, 4, 5, 6];  // Array definition
+let totalsum = addnum(arr);  // Call the function correctly
+console.log(totalsum);  // Output: 21
---
+++ JavaScript/traficlight.js
@@ -0,0 +1,3 @@
+// red - stop yellow - start green - bhaaga
+
+// why i in the loop beacues fornere tell loops condition means itteratiom
