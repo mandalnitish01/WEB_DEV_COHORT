66ee8076a0631ad827f79a0f6695f8221c21b3b4
nitish <mandalnitish570@gmail.com>
2025-02-06 10:18:01 +0530

    ...

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -1,5 +1,4 @@
 // Creating an Array 
-
 // let arr = [
 //     'aman',
 //     'suman',
@@ -7,8 +6,6 @@
 //     'chuck'
 // ]
 // let arr1 = ["aman", "suman", "gagan", "chuck"];
-
-
 // const arr2 = [];
 // arr2[0] = 'aman';
 // arr2[1] = 'suman';
@@ -19,7 +16,6 @@
 
 
 // Using the JavaScript Keyword new
-
 // const arr = new Array('nitish','suresh','mukesh');
 //                     // |
 //                 // this Array must need to define     
@@ -29,10 +25,54 @@
 
 
 // Accessing Array Elements
-
 // const car = ['volvo','fortuner','mercedes','KIA']
 // console.log(car[3])
 // console.log(car[0])
+
+
+
+// Changing an Array Element
+// const car = ['volvo','fortuner','mercedes','KIA']
+// car[1] = "Wagonr" //it menuplate the car[1] array at index 1st; and changed to "Wagonr".
+// console.log(car) //[ 'volvo', 'Wagonr', 'mercedes', 'KIA' ]
+
+
+
+// Converting an Array to a String
+// const car = ['volvo','fortuner','mercedes','KIA']
+// console.log(car.toString()) //volvo,fortuner,mercedes,KIA, here it convert array to string by using toString() function
+
+
+
+// Access full array 
+// <!DOCTYPE html>
+// <html>
+// <body>
+// <h1>JavaScript Arrays</h1>
+
+// <p id="demo"></p>
+
+// <script>
+// const cars = ["Saab", "Volvo", "BMW"];
+// document.getElementById("demo").innerHTML = cars;
+// </script>
+
+// </body>
+// </html>
+
+
+
+
+// Arrays are Objects
+// Arrays are a special type of objects. The typeof operator in JavaScript returns "object" for arrays.
+// But, JavaScript arrays are best described as arrays.
+// Arrays use numbers to access its "elements". In this example, person[0] returns John:
+
+
+
+
+
+
 
 
 
