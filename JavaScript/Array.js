14582f2e5784a787e559344c239649ec8ce79150
nitish <mandalnitish570@gmail.com>
2025-02-06 09:58:02 +0530

    Array and its methods

---
+++ JavaScript/Array.js
@@ -0,0 +1,37 @@
+// Creating an Array 
+
+// let arr = [
+//     'aman',
+//     'suman',
+//     'gagan',
+//     'chuck'
+// ]
+// let arr1 = ["aman", "suman", "gagan", "chuck"];
+
+
+// const arr2 = [];
+// arr2[0] = 'aman';
+// arr2[1] = 'suman';
+// console.log(arr2)
+// console.log(arr2[1])
+
+
+
+
+// Using the JavaScript Keyword new
+
+// const arr = new Array('nitish','suresh','mukesh');
+//                     // |
+//                 // this Array must need to define     
+// console.log(arr)
+
+
+
+
+// Accessing Array Elements
+
+const car = ['volvo','fortuner','mercedes','KIA']
+console.log(car[3])
+console.log(car[0])
+
+
