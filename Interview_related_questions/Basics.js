b6ade66dee6af27b0afb544b251b4293f7203f67
nitish <mandalnitish570@gmail.com>
2025-02-24 17:55:30 +0530

    solve some machine coding basics  interview questions

---
+++ Interview_related_questions/Basics.js
@@ -0,0 +1,18 @@
+console.log(+true) //1 
+console.log(+false) //0
+//conclusion of both prv line, we write 2 console statements and then under the console we write +true in js true refers 1 and false refers 0 and after + js will under true/false as a number
+console.log(+[]) //0
+console.log(+{}) //NaN
+console.log(+null) //0
+console.log(+undefined) //NaN
+console.log(+Infinity) //Infinity
+console.log(+NaN) //NaN
+console.log(+0) // 0
+console.log(+1) //1
+console.log(+2) //2
+console.log(true + true) //2
+console.log(true + false) //1
+console.log(true + null) //1
+console.log(true + undefined) //NaN
+console.log(true + Infinity) //Infinity
+console.log(true + NaN) //NaN
