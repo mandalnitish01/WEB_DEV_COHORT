eb7e5c0e7490c20ac8dcc78fa827a02850c8da03
nitish <mandalnitish570@gmail.com>
2025-02-27 15:48:05 +0530

    Array mwthod in onesort in just 3hr

---
+++ Marathon-classes/Array.js
@@ -0,0 +1,33 @@
+// Array:-  [storing a collection of multiple items under a single variable name]
+
+// Array Rules 
+// Array is a collection of multiple items under a single variable name.
+// Array is an object in JavaScript.
+// Array can store multiple data types.
+
+// Array Declaration
+
+const array = [1,2,3,4,5,76,8,6];
+
+// array methods 
+// 1. push() - add an element at the end of the array
+// 2. pop() - remove an element from the end of the array
+// 3. shift() - remove an element from the beginning of the array
+// 4. unshift() - add an element at the beginning of the array
+// 5. splice() - add or remove elements from the array
+// 6. slice() - returns the selected elements from the array
+// 7. indexOf() - returns the index of the first occurrence of an element in the array
+// 8. lastIndexOf() - returns the index of the last occurrence of an element in the array
+// 9. includes() - returns true if an element is present in the array
+// 11. join() - joins all the elements of the array into a string
+// 12. toString() - converts an array into a string
+// 13. concat() - merge two or more arrays
+// 14. reverse() - reverse the order of the elements in the array
+// 15. sort() - sort the elements of the array
+// 16. filter() - filter the elements of an array based on a condition
+// 17. map() - create a new array with the results of calling a function for every array element
+// 18. reduce() - reduce the array to a single value
+// 19. forEach() - execute a function for each element in the array
+// 20. find() - returns the first element in the array that satisfies a condition
+// 22. some() - check if some elements in the array pass a test
+// 23. every() - check if all elements in the array pass a test
