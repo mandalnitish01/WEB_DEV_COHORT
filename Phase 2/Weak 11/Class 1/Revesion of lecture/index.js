34345c334d9325164e99d0441209ab7c652cbb05
nitish <mandalnitish570@gmail.com>
2025-03-09 12:08:45 +0530

    added node modules

--- Phase 2/Weak 11/Class 1/Revesion of lecture/index.js
+++ Phase 2/Weak 11/Class 1/Revesion of lecture/index.js
@@ -1,1 +1,26 @@
-console.log("hyyy")
+// const express = require('express')
+import express from "express"
+import dotenv from "dotenv"
+import cord from "cors"
+
+
+const app = express()
+dotenv.config()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get("/nitish" , (req,res)=>{
+    res.send("Hyy Nitish!")
+})
+//if you give the route then you myst need to add slace before write the route name
+app.get("/niraj" , (req,res)=>{ //route start with /npm i dotenv
+    res.send("Nyy niraj!")
+})
+
+// console.log(process.env.PORT)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
