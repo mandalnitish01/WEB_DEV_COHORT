69481e9eb9044a3e272d5d2c1969d72b2bfd606c
nitish <mandalnitish570@gmail.com>
2025-04-11 22:01:32 +0530

    auth using openid

--- JavaScript/01_Basics/01_orientation.js
+++ JavaScript/01_Basics/01_orientation.js
@@ -43,3 +43,11 @@ printCoffee(10);
 addTwo(10, 20);
 mutliplyTwo(10*20);
 console.log(mutliplyTwo(10, 40));
+
+
+
+let a = 10;
+a = 20; //let variable value can be redefined 
+const b = 34;
+// b = 45; //but const variable can not be redefined
+console.log(a,b)
